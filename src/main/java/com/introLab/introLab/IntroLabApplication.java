5892819abc40e1a34a33d0f859de014d0c271d11
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-04-24 15:01:40 +0200

    inital commit

---
+++ src/main/java/com/introLab/introLab/IntroLabApplication.java
@@ -0,0 +1,13 @@
+package com.introLab.introLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/introLab/introLab/IntroLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introLab.introLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
